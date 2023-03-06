275bcf3f02be7bd0d7fa5ad8006f8998d7384a75
Ben Sless <ben.sless@gmail.com>
2023-03-06 15:06:16 +0200

    Conditionally cache shapes file and organize benchmarks

--- src/bsless/shapes_bench.clj
+++ src/bsless/shapes_bench.clj
@@ -1,7 +1,9 @@
 (ns bsless.shapes-bench
   (:require
+   [clojure.edn :as edn]
    [criterium.core :as cc]
-   #_[clj-async-profiler.core :as prof])
+   #_[clj-async-profiler.core :as prof]
+   [clojure.java.io :as io])
   (:import
    (java.lang Math)))
 
@@ -9,14 +11,18 @@
 
 (println *compiler-options*)
 
+(def results (atom {}))
+
 (defmacro bench [body]
-  `(let [ret# (cc/quick-benchmark ~body {})
-         est# (:mean ret#)
-         mean# (first est#)
-         [factor# unit#] (cc/scale-time mean#)]
+  `(let [_# (Thread/sleep 9000)
+         ret# (cc/benchmark ~body {})]
      (cc/report-result ret#)
      (println "\n\n")
-     [mean# (cc/format-value mean# factor# unit#)]))
+     ret#))
+
+(defmacro bench!
+  [name body]
+  `(swap! results assoc ~name (bench ~body)))
 
 (defn make-circle [n] {:type :circle :radius (rand n)})
 (defn make-square [n] {:type :square :side (rand n)})
@@ -27,7 +33,14 @@
 
 (defn random-shape [n] ((rand-nth makers) n))
 
-(defonce shapes (into [] (repeatedly (int 4e5) #(random-shape 10))))
+(defonce shapes
+  (let [f (io/file "shapes.edn")]
+    (if (.exists ^java.io.File f)
+      (with-open [r (java.io.PushbackReader. (io/reader f))]
+        (edn/read r))
+      (let [shapes (into [] (repeatedly (int 4e5) #(random-shape 10)))]
+        (spit "shapes.edn" shapes)
+        shapes))))
 
 (defn circle-area [{:keys [radius]}] (* Math/PI radius radius))
 (defn square-area [{:keys [side]}] (* side side))
@@ -46,9 +59,9 @@
   [shapes]
   (transduce (map shape-area-case) + 0 shapes))
 
-(comment)
-(println "Case + transduce")
-(bench (total-area-case-transduce shapes))
+(defn bench-case-transduce []
+  (println "Case + transduce")
+  (bench! "case-transduce" (total-area-case-transduce shapes)))
 ;; => [0.028277429083333333 "28.277429 ms"]
 
 (defn prim-circle-area ^double [shape] (let [radius (unchecked-double (:radius shape))] (* Math/PI radius radius)))
@@ -71,12 +84,14 @@
   [shapes]
   (transduce (map prim-shape-area-case) + 0 shapes))
 
-(println "Case + prims + transduce")
-(bench (prim-total-area-case-transduce shapes))
+(defn bench-case-prims-transduce []
+  (println "Case + prims + transduce")
+  (bench! "case-prims-transduce" (prim-total-area-case-transduce shapes)))
 ;; => [0.0168039305 "16.803931 ms"]
 
-(println "Case + prims + reduce")
-(bench (prim-total-area-case-reduce shapes))
+(defn bench-case-prims-reduce []
+  (println "Case + prims + reduce")
+  (bench! "case-prims-reduce" (prim-total-area-case-reduce shapes)))
 ;; => [0.013422389958333334 "13.422390 ms"]
 
 (defmulti shape-area-multi :type)
@@ -89,8 +104,9 @@
   [shapes]
   (transduce (map shape-area-multi) + 0 shapes))
 
-(println "multimethod + transduce")
-(bench (total-area-multi-transduce shapes))
+(defn bench-multimethod-transduce []
+  (println "multimethod + transduce")
+  (bench! "multimethod-transduce" (total-area-multi-transduce shapes)))
 ;; => [0.030941701208333335 "30.941701 ms"]
 
 (defonce shapes-factors
@@ -113,10 +129,9 @@
   [shapes]
   (transduce (map shape-area-factor) + 0 shapes))
 
-(total-area-factor-transduce shapes-factors)
-
-(println "factor + transduce")
-(bench (total-area-factor-transduce shapes-factors))
+(defn bench-factors-transduce []
+  (println "factor + transduce")
+  (bench! "factors-transduce" (total-area-factor-transduce shapes-factors)))
 ;; => [0.0138719610625 "13.871961 ms"]
 
 (defprotocol PArea
@@ -144,16 +159,20 @@
   (transduce (map -area) + 0 shapes))
 
 
-(println "protocol + transduce")
-(bench (total-area-protocol-transduce precord-shapes))
+(defn bench-protocols-transduce []
+  (println "protocol + transduce")
+  (bench! "protocols-transduce" (total-area-protocol-transduce precord-shapes)))
 ;; => [0.012866039729166667 "12.866040 ms"]
+
 (defrecord Shape [factor width height])
 
 (defonce record-shapes-factors (into [] (map map->Shape) shapes-factors))
 
-(println "shape record + kw access + transduce")
-(bench (total-area-factor-transduce record-shapes-factors))
+(defn bench-shape-record-kw-access-transduce []
+  (println "shape record + kw access + transduce")
+  (bench! "shape-record-kw-access-transduce" (total-area-factor-transduce record-shapes-factors)))
 ;; => [0.016156389 "16.156389 ms"]
+
 (defn record-shape-area [^Shape shape]
   (* (.factor shape) (.width shape) (.height shape)))
 
@@ -161,9 +180,11 @@
   [shapes]
   (transduce (map record-shape-area) + 0 shapes))
 
-(println "shape record + member access + transduce")
-(bench (total-area-record-transduce record-shapes-factors))
+(defn bench-shape-record-member-access-transduce []
+  (println "shape record + member access + transduce")
+  (bench! "shape-record-member-access-transduce" (total-area-record-transduce record-shapes-factors)))
 ;; => [0.0130048725 "13.004873 ms"]
+
 (definterface IPrimShape
   (^double getArea []))
 
@@ -181,16 +202,18 @@
   [shapes]
   (transduce (map prim-record-shape-area) + 0 shapes))
 
-(println "prim shape record + member access + transduce")
-(bench (total-area-prim-record record-prim-shapes-factors))
+(defn bench-prim-shape-record-member-access-transduce []
+  (println "prim shape record + member access + transduce")
+  (bench! "prim-shape-record-member-access-transduce" (total-area-prim-record record-prim-shapes-factors)))
 ;; => [0.004860275587301587 "4.860276 ms"]
 
 (defn total-area-prim-record-reduce
   [shapes]
   (reduce (fn ^double [^double acc shape] (+ acc (prim-record-shape-area shape))) 0 shapes))
 
-(println "prim shape record + member access + reduce")
-(bench (total-area-prim-record-reduce record-prim-shapes-factors))
+(defn bench-prim-shape-record-member-access-reduce []
+  (println "prim shape record + member access + reduce")
+  (bench! "prim-shape-record-member-access-reduce" (total-area-prim-record-reduce record-prim-shapes-factors)))
 ;; => [0.002313144886524823 "2.313145 ms"]
 
 (defn get-area ^double [^IPrimShape shape]
@@ -199,16 +222,18 @@
 (defn total-area-prim-record-method [shapes]
   (transduce (map get-area) + 0 shapes))
 
-(println "prim shape record + interface call + transduce")
-(bench (total-area-prim-record-method record-prim-shapes-factors))
+(defn bench-prim-shape-record-interface-method-transduce []
+  (println "prim shape record + interface call + transduce")
+  (bench! "prim-shape-record-interface-method-transduce" (total-area-prim-record-method record-prim-shapes-factors)))
 ;; => [0.004738504543478262 "4.738505 ms"]
 
 (defn total-area-prim-record-method-reduce
   [shapes]
   (reduce (fn ^double [^double acc shape] (+ acc (get-area shape))) 0 shapes))
 
-(println "prim shape record + interface call + reduce")
-(bench (total-area-prim-record-method-reduce record-prim-shapes-factors))
+(defn bench-prim-shape-record-interface-method-reduce []
+  (println "prim shape record + interface call + reduce")
+  (bench! "prim-shape-record-interface-method-reduce" (total-area-prim-record-method-reduce record-prim-shapes-factors)))
 ;; => [0.002061013670068027 "2.061014 ms"]
 
 (import java.util.Iterator)
@@ -221,8 +246,9 @@
         (recur (unchecked-add sum (prim-record-shape-area (.next it))))
         sum))))
 
-(println "prim shape record + loop")
-(bench (total-area-prim-record-loop record-prim-shapes-factors))
+(defn bench-prim-shape-record-iter-loop []
+  (println "prim shape record + loop")
+  (bench! "prim-shape-record-iter-loop" (total-area-prim-record-loop record-prim-shapes-factors)))
 ;; => [0.0011672441529411767 "1.167244 ms"]
 
 (set! *warn-on-reflection* true)
@@ -234,8 +260,9 @@
 (defn total-area-stream ^double [shapes]
   (.sum (.mapToDouble (.stream ^java.util.Collection shapes) df)))
 
-(println "prim shape record + stream")
-(bench (total-area-stream record-prim-shapes-factors))
+(defn bench-prim-shape-record-stream []
+  (println "prim shape record + stream")
+  (bench! "prim-shape-record-stream" (total-area-stream record-prim-shapes-factors)))
 ;; => [0.003212101953125 "3.212102 ms"]
 #_
 (prof/serve-files 7777)
@@ -258,8 +285,9 @@
         (recur (unchecked-add sum (prim-record-shape-area (aget ^objects shapes i)))
                (unchecked-inc i))))))
 
-(println "prim shape record + array loop")
-(bench (total-area-prim-record-loop-array record-prim-shapes-factors-array))
+(defn bench-prim-shape-record-array-loop []
+  (println "prim shape record + array loop")
+  (bench! "prim-shape-record-array-loop" (total-area-prim-record-loop-array record-prim-shapes-factors-array)))
 ;; => [7.437256957070708E-4 "743.725696 µs"]
 #_
 (time
@@ -289,6 +317,26 @@
 
 (def type-prim-shapes-factors-arr (into-array PrimShapeType type-prim-shapes-factors))
 
-(println "prim shape type + array loop")
-(bench (total-area-prim-type-loop-array type-prim-shapes-factors-arr))
+(defn bench-prim-shape-type-array-loop []
+  (println "prim shape type + array loop")
+  (bench! "prim-shape-type-array-loop" (total-area-prim-type-loop-array type-prim-shapes-factors-arr)))
 ;; => [4.1439404928664074E-4 "414.394049 µs"]
+
+(defn bench-all []
+  (bench-case-transduce)
+  (bench-case-prims-transduce)
+  (bench-case-prims-reduce)
+  (bench-multimethod-transduce)
+  (bench-factors-transduce)
+  (bench-protocols-transduce)
+  (bench-shape-record-kw-access-transduce)
+  (bench-shape-record-member-access-transduce)
+  (bench-prim-shape-record-member-access-transduce)
+  (bench-prim-shape-record-member-access-reduce)
+  (bench-prim-shape-record-interface-method-transduce)
+  (bench-prim-shape-record-interface-method-reduce)
+  (bench-prim-shape-record-iter-loop)
+  (bench-prim-shape-record-stream)
+  (bench-prim-shape-record-array-loop)
+  (bench-prim-shape-type-array-loop)
+  (spit (str "results-" (System/getProperty "java.vm.version") ".edn") @results))
